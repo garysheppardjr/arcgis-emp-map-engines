f245a86fe522401a3fb2ddf7176cc40082aea599
Gary Sheppard <gsheppard@esri.com>
2015-09-09 09:27:40 -0400

    Experimental methods and properties for engine interface

--- engines/emp-engine-arcgis-3d/arcgis-map-engine-3d.js
+++ engines/emp-engine-arcgis-3d/arcgis-map-engine-3d.js
@@ -16,5 +16,63 @@
 function arcgisMapEngine3d() {
   var engineInterface = emp.map.createEngineTemplate();
   
+  /***********************************************************************
+   * TODO everything following this (except the return statement) is for testing
+   * and experimenting. So far, none of these methods seem to get called or have
+   * an effect on EMP or CPCE.
+   */
+  
+  engineInterface.implementation.displayName = "ArcGIS 3D";
+  
+  engineInterface.capabilities.mapType.type3D = true;
+  engineInterface.capabilities.formats.ArcGIS = true;
+  
+  engineInterface.initialize.succeed = function (args) {
+    console.log("success!");
+  };
+  
+  engineInterface.initialize.fail = function (args) {
+    console.log("failure");
+  };
+  
+  engineInterface.status.get = function (transaction) {
+    console.log("get");
+  };
+  
+  engineInterface.state.reset = function (transaction) {
+    console.log("reset");
+  };
+  
+  engineInterface.state.destroy = function () {
+    console.log("destroy");
+  };
+  
+  engineInterface.visibility.set = function (transaction) {
+    console.log("visibility.set");
+  };
+  
+  engineInterface.overlay.add = function (transaction) {
+    console.log("overlay.add");
+  };
+  
+  engineInterface.overlay.remove = function (transaction) {
+    console.log("overlay.remove");
+  };
+  
+  engineInterface.overlay.update = function (transaction) {
+    console.log("overlay.update");
+  };
+  
+  engineInterface.overlay.clear = function (transaction) {
+    console.log("overlay.clear");
+  };
+  
+  engineInterface.navigation = engineInterface.navigation || {};
+  engineInterface.navigation.enable = function(transaction) {
+    console.log("navigation.enable");
+  };
+  
+  /***********************************************************************/
+  
   return engineInterface;
 }
